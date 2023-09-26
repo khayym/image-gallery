0d159d7b76f29a8a96946653e8e0697c4b52c323
khayym <xeyyam.kerimov@gmail.com>
2023-09-26 16:50:25 +0400

    configs setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+          '.svg',
+        ],
+        alias: {
+          components: './src/components',
+          icons: './assets/icons',
+          images: './assets/images',
+          configs: './src/configs',
+          helpers: './src/helpers',
+          screens: './src/screens',
+          store: './src/store',
+          theme: './src/theme',
+          types: './src/types',
+          constants: './src/constants',
+        },
+      },
+    ],
+  ],
 };
